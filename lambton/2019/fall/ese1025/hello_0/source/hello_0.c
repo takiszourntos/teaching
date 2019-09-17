cf79fddfdaac3ebf98f868fd800a9662a8ae7f40
Takis Zourntos <takis.zourntos@emads.org>
2019-09-17 18:10:37 -0400

    first commit for ese1025

---
+++ lambton/2019/fall/ese1025/hello_0/source/hello_0.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : hello_0.c
+ Author      : takis
+ Version     :
+ Copyright   : (c) 2019 emad studio inc.
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+
+int main()
+{
+	printf("Hello, from Lambton College in Toronto!\n"); /* greeting sent to console */
+	return 0;
+}
