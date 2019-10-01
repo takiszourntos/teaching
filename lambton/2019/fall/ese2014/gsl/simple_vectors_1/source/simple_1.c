fa45281b1c13e6ba84cf47411cd0e70521768160
Takis Zourntos <takis.zourntos@emads.org>
2019-10-01 13:38:54 -0400

    got basic simple_1.c working

--- lambton/2019/fall/ese2014/gsl/simple_vectors_1/source/simple_1.c
+++ lambton/2019/fall/ese2014/gsl/simple_vectors_1/source/simple_1.c
@@ -8,6 +8,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <gsl/gsl_vector_double.h> /* using double precision */
+#include <gsl/gsl_rng.h>
+#include <gsl/gsl_randist.h>
 
 /* simple Fibonancci sequence generator, using recursion */
 size_t fib(size_t k)
@@ -28,23 +30,38 @@ size_t fib(size_t k)
 
 int main()
 {
-	/* vector parameters */
-	size_t		N=10; /* index type, vector sizes */
-	gsl_vector *a = gsl_vector_alloc(N); /* allocate vector of size N */
-	gsl_vector *b = gsl_vector_calloc(N); /* allocate vector of size N, initialize entries to zero */
+	/* parameters */
+		/* vectors */
+		size_t		N=10; /* index type, vector sizes */
+		gsl_vector *a = gsl_vector_alloc(N); /* allocate vector from heap of size N */
+		gsl_vector *b = gsl_vector_alloc(N); /* allocate vector from heap of size N */
+		gsl_vector *c = gsl_vector_calloc(N); /* allocate vector of size N but initialize entries to zero */
+
+		/* random numbers */
+		const gsl_rng_type *T;
+		gsl_rng *r; /* handle for our random number generator */
+
+	/* set up random number generation */
+	gsl_rng_env_setup();
+	T = gsl_rng_default;
+	r = gsl_rng_alloc(T);
 
 	/* initialize vectors */
 	for (size_t i = 0; i != N; ++i)
 	{
-
-		gsl_vector_set(a, i, 1.23 + i);
+		gsl_vector_set(a, i, fib(i)); /* set element i of vector a to Fibonacci number i */
+		gsl_vector_set(b, i, gsl_ran_flat(r, -1.0, +1.0));
+//		gsl_vector_set(b, i, 1.0);
+		gsl_vector_add(c, a); /* c += a */
+		gsl_vector_add(c, b); /* c += b */
+		printf("i=%d, a(i)=%f, b(i)=%f, c(i)=%f\n", i,
+													gsl_vector_get(a, i),
+													gsl_vector_get(b, i),
+													gsl_vector_get(c, i));
 	}
-
-
-
-
-
-
-
+	/* de-allocate memory */
+	gsl_vector_free(a);
+	gsl_vector_free(b);
+	gsl_vector_free(c);
 	return EXIT_SUCCESS;
 }
