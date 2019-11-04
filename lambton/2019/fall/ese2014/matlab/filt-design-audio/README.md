f588c6a48523bf5b0bc22a4074314b25252859ad
Takis Zourntos <takis.zourntos@emads.org>
2019-11-04 15:00:58 -0500

    added filter design examples/inspiration for students

---
+++ lambton/2019/fall/ese2014/matlab/filt-design-audio/README.md
@@ -0,0 +1,5 @@
+## analog filter prototyping for audio application
+
+- specs: 2-dB passband ripple, 80-dB stopband attenuation, transition band from 16-kHz to 20-kHz
+
+![elliptical is much lower order](filt_compare_0.png)
---
+++ lambton/2019/fall/ese2014/matlab/filt-design-audio/filt0.m
@@ -0,0 +1,70 @@
+%%
+% analog filter design
+%
+% specs:
+%
+% 
+% Passband Ripple: 2 dB
+% Stopband Attenuation: 80 dB
+% Fpass = 16 kHz
+% Fstop = 20 kHz
+
+%
+clc;
+close all;
+clear;
+spec_pbr_pos = 1; % upper ripple limit as a unitless quantity
+spec_pbr_neg = 10^(-2/20); % lower ripple limit as a unitless quantity
+spec_sba = 10^(-80/20); % stopband attenuation, converted from dB
+
+Wrange = linspace(0,2*pi*50000,1000000); % linear frequency range for plotting
+Frange = (1/(2*pi))*Wrange;
+Ypbrpos = spec_pbr_pos*ones(size(Wrange));
+Ypbrneg = spec_pbr_neg*ones(size(Wrange));
+Ysba = spec_sba*ones(size(Wrange));
+
+
+%% Butterworth Design
+    % design attempt
+    Nbw = 43;
+    Wnbw = 2*pi*16100;
+    [numHbw, denHbw] = butter(Nbw,Wnbw,'s');
+    Hbw = tf(numHbw,denHbw);
+    [Ybw_mag,Ybw_ph] = bode(Hbw, Wrange);
+    Ybw_mag=reshape(Ybw_mag,[1 max(size(Ybw_mag))]);
+    Ybw_ph=reshape(Ybw_ph,[1 max(size(Ybw_ph))]);
+    
+    % plot response with specs superimposed
+    figure; plot(Frange, 20*log10(Ybw_mag)); grid;
+    title('Butterworth Analog Filter Response (N=43)');
+    xlabel('freq (kHz)');
+    ylabel('response (dB)');
+    % plot specs/constraints for comparison
+    hold on; plot(Frange,20*log10(Ypbrpos),'r-', Frange,20*log10(Ypbrneg),'r-',Frange,20*log10(Ysba),'r-');
+    xline(16000,'r-'); xline(20000,'r-');
+    
+%% Elliptical Design
+    % design attempt
+    Ne = 8;
+    Rpe = 2;
+    Rse = 80;
+    Wpe = 2*pi*16000;
+    [numHe, denHe] = ellip(Ne,Rpe,Rse,Wpe,'s');
+    He = tf(numHe,denHe);
+    [Ye_mag,Ye_ph] = bode(He, Wrange);
+    Ye_mag=reshape(Ye_mag,[1 max(size(Ye_mag))]);
+    Ye_ph=reshape(Ye_ph,[1 max(size(Ye_ph))]);
+
+    % plot response with specs superimposed
+    figure; plot(Frange, 20*log10(Ye_mag)); grid;
+    title('Elliptical Analog Filter Response (N=8)');
+    xlabel('freq (kHz)');
+    ylabel('response (dB)');
+    % plot specs/constraints for comparison
+    hold on; plot(Frange,20*log10(Ypbrpos),'r-', Frange,20*log10(Ypbrneg),'r-',Frange,20*log10(Ysba),'r-');
+    xline(16000,'r-'); xline(20000,'r-');
+
+    
+
+
+
