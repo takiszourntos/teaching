8e48c0beac266478dc406ed130ec4f6c11c7ba77
Takis Zourntos <47870726+takiszourntos@users.noreply.github.com>
2019-12-17 10:32:53 -0500

    Update buffers.c

--- lambton/2019/fall/ese2025/c/mcu/dsp_0/source/buffers.c
+++ lambton/2019/fall/ese2025/c/mcu/dsp_0/source/buffers.c
@@ -7,20 +7,20 @@
  * 	time.
  *
  *
- *		 	  index	   element
+ *		 	      index	   element
  *				0		?
  *				1		?
  *				2		?
  *				.		.
  *				.		.
  *				.		.
- * b.offset-->	r		x(n-(K-1))
+ * b.offset-->			r		x(n-(K-1))
  *				r+1		x(n-(K-2))
  *				.		.
  *				.		.
  *				.		.
  *				s-1		x(n-1)
- * b.current-->	s   	x(n)
+ * b.current-->			s   		x(n)
  *				.		.
  *				.		.
  *				.		.
