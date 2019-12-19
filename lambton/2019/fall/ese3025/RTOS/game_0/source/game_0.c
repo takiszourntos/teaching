5c0bd9852c1fb7cf5f03037a250fcda3a786142d
Takis Zourntos <takis.zourntos@emads.org>
2019-12-19 07:36:26 -0500

    converging on gaming engine code

--- lambton/2019/fall/ese3025/RTOS/game_0/source/game_0.c
+++ lambton/2019/fall/ese3025/RTOS/game_0/source/game_0.c
@@ -9,11 +9,11 @@
 */
 
 #if defined (__USE_LPCOPEN)
-#if defined(NO_BOARD_LIB)
-#include "chip.h"
-#else
-#include "board.h"
-#endif
+	#if defined(NO_BOARD_LIB)
+		#include "chip.h"
+	#else
+		#include "board.h"
+	#endif
 #endif
 
 #include <cr_section_macros.h>
@@ -27,47 +27,93 @@
 // TODO: insert other definitions and declarations here
 
 /* global variables */
-volatile	queue_t	q;
+volatile	queue_t	q; /* UART queue */
 volatile	game_t	this_game;
 ui_t		user;
 
-/*
+/********************************************************************
+ *
  * PRIVATE FUNCTIONS
+ *
+ ********************************************************************/
+
+/*
+ * function to initialize game at very beginning
  */
 static void prvInitGame(void)
 {
+	/* new game parameters */
 	this_game.score = 0;
-	this_game.playerID = {'A','A','A','\0'};
+	this_game.playerID="AAA";
 	this_game.level = 1;
-	this_game.aliens = (go_t *) pvPortMalloc(MAXALIENS*sizeof(go_t));
-	this_game.poohs = (go_t *) pvPortMalloc(MAXPOOHS*sizeof(go_t));
-	this_game.expungers = (go_t *) pvPortMalloc(MAXEXPUNGERS*sizeof(go_t));
-	this_game.babies = (go_t *) pvPortMalloc(MAXBABIES*sizeof(go_t));
-	this_game.kitties = (go_t *) pvPortMalloc(MAXKITTIES*sizeof(go_t));
-	this_game.players = (go_t *) pvPortMalloc(MAXPLAYERS*sizeof(go_t));
+	/* allocate head for each GO list */
+	this_game.aliens = (go_t *) pvPortMalloc(sizeof(go_t));
+	this_game.poohs = (go_t *) pvPortMalloc(sizeof(go_t));
+	this_game.expungers = (go_t *) pvPortMalloc(sizeof(go_t));
+	this_game.babies = (go_t *) pvPortMalloc(sizeof(go_t));
+	this_game.kitties = (go_t *) pvPortMalloc(sizeof(go_t));
+	this_game.players = (go_t *) pvPortMalloc(sizeof(go_t));
 }
 
-int main(void) {
+/*
+ * function to reset game board, called by vRunGameTask(), after scheduler has started
+ */
+static void prvResetBoard(void)
+{
+	/* send command to clear the game screen/board and wait for 5 seconds */
+	taskENTER_CRITICAL();
+	{
+		prvUARTSend("C:clc"); /* command: clear console */
+		vTaskDelay(configTICKRATEHZ*5); /* wait for a few seconds */
+		prvUARTSend("D:emad studio inc. presents: ");
+		vTaskDelay(configTICKRATEHZ*2);
+		prvUARTSend("D:aliens & babies : at the daycare!");
+		vTaskDelay(configTICKRATEHZ*5);
+	}
+	taskEXIT_CRITICAL();
+}
 
-#if defined (__USE_LPCOPEN)
-    // Read clock settings and update SystemCoreClock variable
-    SystemCoreClockUpdate();
-#if !defined(NO_BOARD_LIB)
-    // Set up and initialize all required blocks and
-    // functions related to the board hardware
-    Board_Init();
-    // Set the LED to the state of "On"
-    Board_LED_Set(0, true);
-#endif
-#endif
+/*
+ * function to initialize hardware, run at the very beginning
+ */
+static void prvSetupHardware(void)
+{
+	#if defined (__USE_LPCOPEN)
+		// Read clock settings and update SystemCoreClock variable
+		SystemCoreClockUpdate();
+	#if !defined(NO_BOARD_LIB)
+		// Set up and initialize all required blocks and
+		// functions related to the board hardware
+		Board_Init();
+		// Set the LED to the state of "On"
+		Board_LED_Set(0, true);
+	#endif
+	#endif
+}
 
-    // TODO: insert code here
+/*
+ *
+ * MAIN FUNCTION
+ *
+ */
+int main(void)
+{
+	char single_player_mode[30] = "single-player mode";
 
-    // Force the counter to be placed into memory
-    volatile static int i = 0 ;
-    // Enter an infinite loop, just incrementing a counter
-    while(1) {
-        i++ ;
-    }
-    return 0 ;
+	/* hardware init */
+	prvSetupHardware();
+
+	/* new game init */
+	prvInitGame();
+
+	/* start game */
+	xTaskHandle pvRunGameTaskHandle; /* handle to supervisory task */
+	xTaskCreate(vRunGameTask, "Supervisory Game Task", 4096,
+				single_player_mode, &pvRunGameTaskHandle, RUN_GAME_PRIORITY);
+
+	/* relinquish control to scheduler */
+	vTaskStartScheduler();
+
+	/* return error code if scheduler exits */
+    return 1;
 }
---
+++ lambton/2019/fall/ese3025/RTOS/game_0/source/libgametasks.c
@@ -0,0 +1,22 @@
+/*
+ * libgametasks.c
+ *
+ *  Created on: Dec. 19, 2019
+ *      Author: takis
+ */
+
+#include <stdlib.h>
+
+
+void vRunGameTask(void *pvParams)
+{
+	go_t	*pW=NULL;
+	xTaskHandle	pvImpactsTask;
+
+	while (1)
+	{
+
+	}
+
+}
+
