4bc411875bb7d198f3ddda162fb78cf12b1a4127
Takis Zourntos <takis.zourntos@emads.org>
2020-02-20 14:09:00 -0500

    formatting changes to pthreads_1 and pthreads_2

--- lambton/2020/winter/ese3025/POSIX/pthreads_2/source/pthreads_2.c
+++ lambton/2020/winter/ese3025/POSIX/pthreads_2/source/pthreads_2.c
@@ -15,117 +15,117 @@
 /*
  * 	global variables
  */
-int	num_ints=0;	// current number of integers entered by user
-int	done=0;		// flag indicating completion of user data entry
-float	average=0.0;	// global storage for the average
+int num_ints = 0;	// current number of integers entered by user
+int done = 0;		// flag indicating completion of user data entry
+float average = 0.0;	// global storage for the average
 
 /*
  *	function to display the set of numbers periodically
  */
-void	disp_sorted(int *data)
+void
+disp_sorted (int *data)
 {
-	if (num_ints != 0)
+  if (num_ints != 0)
+    {
+      printf ("the current (sorted) integer set is: ");
+      for (int i = 0; i != num_ints; ++i)
 	{
-		printf("the current (sorted) integer set is: ");
-		for (int i=0; i!=num_ints; ++i)
-		{
-			printf("%d ", data[i]);
-		}
-		printf("\n");
+	  printf ("%d ", data[i]);
 	}
-	return;              	// return the pointer to x
+      printf ("\n");
+    }
+  return;              	// return the pointer to x
 }
 
 /*
  *	thread function to display the average of the set of numbers periodically
  */
-void 	disp_avg(float x)
+void
+disp_avg (float x)
 {
-	printf("the current average is: %f\n", x);
-	return;
+  printf ("the current average is: %f\n", x);
+  return;
 }
-
 
 /*
  *	thread function computes average of current set
  */
-void	*avg_thread(void *arr)
+void*
+avg_thread (void *arr)
 {
-	int		*x = (int *) arr;	// cast the passed data to integer type
-	int		sum;				// storage for the sum
+  int *x = (int*) arr;	// cast the passed data to integer type
+  int sum;				// storage for the sum
 
-	while (done==0)
+  while (done == 0)
+    {
+      sum = 0;
+      // add up the integer set
+      for (int i = 0; i != num_ints; ++i)
 	{
-		sum=0;
-		// add up the integer set
-		for (int i=0; i!=num_ints; ++i)
-		{
-			sum += x[i];
-		}
-		// calculate average, if possible
-		if (num_ints !=0)
-		{
-			average = (float) (sum/num_ints);
-		}
+	  sum += x[i];
 	}
-	return x; // return the set, unchanged
+      // calculate average, if possible
+      if (num_ints != 0)
+	{
+	  average = (float) (sum / num_ints);
+	}
+    }
+  return x; // return the set, unchanged
 }
 
 /*
  * thread function sorts data set in its current set
  */
-void	*bubb_sort_thread(void *arr)
+void*
+bubb_sort_thread (void *arr)
 {
-	int		*x = (int *) arr;	// cast the passed data to integer type
-	int		swaps; 			// a flag indicating if a swap has occurred (if none occur, this set is sorted)
-	int		temp;
+  int *x = (int*) arr;	// cast the passed data to integer type
+  int swaps; // a flag indicating if a swap has occurred (if none occur, this set is sorted)
+  int temp;
 
-	//while (num_ints != MAXINTS)
-	while (done==0)
+  //while (num_ints != MAXINTS)
+  while (done == 0)
+    {
+      if (num_ints != 0)
 	{
-		if (num_ints != 0)
+	  swaps = 1;
+	  while (swaps == 1)
+	    {
+	      swaps = 0; // if a swap occurs, this flag toggled
+	      for (int i = 0; i != (num_ints - 1); ++i)
 		{
-			swaps = 1;
-			while (swaps==1)
-			{
-				swaps = 0; // if a swap occurs, this flag toggled
-				for (int i=0; i!=(num_ints-1); ++i)
-				{
-					if (x[i] > x[i+1]) // elements must be in ascending order, if not ...
-					{	// ... then swap!
-						temp 	= x[i];
-						x[i] 	= x[i+1];
-						x[i+1] 	= temp;
-						swaps  	= 1; // a swap has occurred
-					}
-				}
-			}
+		  if (x[i] > x[i + 1]) // elements must be in ascending order, if not ...
+		    {	// ... then swap!
+		      temp = x[i];
+		      x[i] = x[i + 1];
+		      x[i + 1] = temp;
+		      swaps = 1; // a swap has occurred
+		    }
 		}
+	    }
 	}
-	return x; // return the set, sorted
+    }
+  return x; // return the set, sorted
 }
 
-int main()
+int
+main ()
 {
-    pthread_t	thread_calc_1;	// our handle for the averaging thread
-    pthread_t	thread_calc_2;	// our handle for the sorting thread
-    int		set[MAXINTS]; 	// storage for our numbers
+  pthread_t thread_calc_1;	// our handle for the averaging thread
+  pthread_t thread_calc_2;	// our handle for the sorting thread
+  int set[MAXINTS]; 	// storage for our numbers
 
-
-    // initialize set to zero
-    for (int i=0; i != MAXINTS; ++i)
+  // initialize set to zero
+  for (int i = 0; i != MAXINTS; ++i)
     {
-    	set[i]=0;
+      set[i] = 0;
     }
 
-    // create threads
+  // create threads
 
-    // main loop (get user input)
+  // main loop (get user input)
 
-    done=1; // user has finished entering data
-    return 0;
+  done = 1; // user has finished entering data
+  return 0;
 }
-
-
-
 
