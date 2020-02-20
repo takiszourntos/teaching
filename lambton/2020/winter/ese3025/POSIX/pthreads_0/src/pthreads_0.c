264e36ab9d66e8d4220b929a7187204e5baec749
Takis Zourntos <takis.zourntos@emads.org>
2020-02-20 13:13:33 -0500

    vain formatting done

--- lambton/2020/winter/ese3025/POSIX/pthreads_0/src/pthreads_0.c
+++ lambton/2020/winter/ese3025/POSIX/pthreads_0/src/pthreads_0.c
@@ -5,7 +5,7 @@
  Version     : 0.1
  Copyright   : (C) 2019 emad studio inc.
  Description : simple pthread example with two threads (including main thread)
- 	 	 	   in C, Ansi-style
+ in C, Ansi-style
  ================================================================================
  */
 
@@ -16,37 +16,40 @@
 
 // This is the thread function that will execute when the thread is created
 //  it passes and receives data by void pointers
-void *threadFunction(void *value)
+void*
+threadFunction (void *value)
 {
-	int *x = (int *)value;    //cast the data passed to an int pointer
-	while(*x<5)
-	{              //while the value of x is less than 5
-		usleep(1000);         //sleep for 1ms - encourage main thread
-		(*x)++;               //increment the value of x by 1
-	}
-	return x;                 //return the pointer x (as a void*)
+  int *x = (int*) value;    //cast the data passed to an int pointer
+  while (*x < 5)
+    {              //while the value of x is less than 5
+      usleep (1000);         //sleep for 1ms - encourage main thread
+      (*x)++;               //increment the value of x by 1
+    }
+  return x;                 //return the pointer x (as a void*)
 }
 
-int main() {
-    int x=0, y=0;
-    pthread_t thread;         //this is our handle to the pthread
-    // create the thread, pass the reference, address of the function and data
-    // pthread_create() returns 0 on the successful creation of a thread
-    if(pthread_create(&thread, NULL, &threadFunction, &x)!=0)
+int
+main ()
+{
+  int x = 0, y = 0;
+  pthread_t thread;         //this is our handle to the pthread
+  // create the thread, pass the reference, address of the function and data
+  // pthread_create() returns 0 on the successful creation of a thread
+  if (pthread_create (&thread, NULL, &threadFunction, &x) != 0)
     {
-    	printf("Failed to create the thread\n");
-    	return 1;
+      printf ("Failed to create the thread\n");
+      return 1;
     }
-    // at this point the thread was created successfully
-    while(y<5)
+  // at this point the thread was created successfully
+  while (y < 5)
     {               // loop and increment y, displaying values
-    	printf("The value of x=%d and y=%d \n",x,y);
-    	++y;
-    	usleep(1000);         // encourage the pthread to run
+      printf ("The value of x=%d and y=%d \n", x, y);
+      ++y;
+      usleep (1000);         // encourage the pthread to run
     }
-    void* result;             // OPTIONAL: receive data back from pthread
-    pthread_join(thread, &result);   // allow the pthread to complete
-    int *z = (int *) result;         // cast from void* to int* to get z
-    printf("Final: x=%d, y=%d and z=%d\n",x,y,*z);
-    return EXIT_SUCCESS;
+  void *result;             // OPTIONAL: receive data back from pthread
+  pthread_join (thread, &result);   // allow the pthread to complete
+  int *z = (int*) result;         // cast from void* to int* to get z
+  printf ("Final: x=%d, y=%d and z=%d\n", x, y, *z);
+  return EXIT_SUCCESS;
 }
