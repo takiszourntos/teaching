1ec5d69fac20aa71545ff739b5f88604eb3183fb
Takis Zourntos <takis.zourntos@emads.org>
2020-03-27 14:00:24 -0400

    added part 2 of crosstol how-to for capstone

---
+++ lambton/2020/winter/ese4009/workspace/bbb_proj_0/source/hello_ide.cpp
@@ -0,0 +1,17 @@
+/*
+ * hello_ide.cpp
+ *
+ *  Created on: Mar. 27, 2020
+ *      Author: takis
+ */
+
+#include <iostream>
+
+int main(void)
+{
+	std::cout << "Hello World!" << std::endl;
+
+	return 0;
+}
+
+
