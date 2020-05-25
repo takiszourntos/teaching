4a379f108dde231a3c85ed1043c85c968e33e62f
Takis Zourntos <takis.zourntos@emads.org>
2020-05-25 15:12:02 -0400

    updates to ese2025 summer 2020 folder

---
+++ lambton/2020/summer/ese2025/week_1/c/find_largest/my_includes/my_func.h
@@ -0,0 +1,15 @@
+/*
+ * my_func.h
+ *
+ *  Created on: May 25, 2020
+ *      Author: takis
+ */
+
+#ifndef MY_FUNC_H_
+#define MY_FUNC_H_
+
+#include<stdlib.h>
+
+int find_largest_int(int *, size_t);
+
+#endif /* MY_FUNC_H_ */
