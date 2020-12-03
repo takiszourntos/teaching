235ebfd2eb97bf345260eb2c9e2c9c292f5accf5
takiszourntos <takis.zourntos@emads.org>
2020-12-02 23:17:59 -0500

    working version of class template project!

--- lambton/2020/fall/ese3005/class_10/workspace/proj_class_expt_templ/Debug/proj_source/students.d
+++
@@ -1,4 +0,0 @@
-proj_source/students.o: ../proj_source/students.cpp \
- /home/takis/Identities/takiszourntos/teaching/lambton/2020/fall/ese3005/class_10/workspace/proj_class_expt_templ/proj_includes/students.h
-
-/home/takis/Identities/takiszourntos/teaching/lambton/2020/fall/ese3005/class_10/workspace/proj_class_expt_templ/proj_includes/students.h:
