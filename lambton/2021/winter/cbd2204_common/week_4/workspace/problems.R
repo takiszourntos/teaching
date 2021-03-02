c1e94b9fb803ac7a26f581c0d17729396a97df0a
takiszourntos <takis.zourntos@emads.org>
2021-03-01 21:03:53 -0500

    updates to 3005 teaching plan, cbd2204, week 4

---
+++ lambton/2021/winter/cbd2204_common/week_4/workspace/problems.R
@@ -0,0 +1,24 @@
+# use the tidyverse library
+library(tidyverse)
+
+# create a dataframe out of tidyverse's mpg data
+datampg <- mpg
+
+# create vectors to access various variables
+x_displ <- datampg$displ
+y_cty <- datampg$cty
+z_year <- datampg$year
+
+# question 1: what are the mean, median and first & third quartiles for 
+# each of these variables?
+
+# question 2: are displacement and fuel economy correlated? Please
+# provide a quantitative answer
+
+# question 3: are engines getting smaller each year? How can you show
+# the relationship between engine size and model year?
+
+# question 4: is manual transmission more fuel efficient than automatic 
+# transmission?
+
+# question 5: which manufacturer makes the most fuel efficient cars?
